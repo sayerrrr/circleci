e76a168aaaff7e18542ad5b10575851adbd73add
Sayer <sayer@worldtree.io>
2021-03-08 20:45:40 -0800

    remove test

--- main.tf
+++ main.tf
@@ -18,32 +18,7 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
 
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-data "template_file" "circleci_policy" {
-  template = file("access_policy.json")
-  vars = {
-    s3_bucket_arn = aws_s3_bucket.bucket.arn
-  }
-}
-
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-}
-
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
